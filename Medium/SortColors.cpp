c3a7c6d5cca637032853c0cca7232a3f23b46531
Yash Borkar <imyashb00@gmail.com>
2024-07-15 22:59:56 +0530

    Update SortColors.cpp

--- Medium/SortColors.cpp
+++ Medium/SortColors.cpp
@@ -5,22 +5,16 @@ public:
     void sortColors(vector<int>& nums) {
     // 3 Pointer Approach
     int l=0,m=0,h=nums.size()-1;
-    while(m<=h)
-    {
-        if(nums[m]==0)
-        {
+    while(m<=h){
+        if(nums[m]==0){
             swap(nums[l],nums[m]);
             l++;
             m++;
         }
-
-        else if(nums[m]==1)
-        {
+        else if(nums[m]==1){
             m++;
         }
-
-        else
-        {
+        else{
             swap(nums[h],nums[m]);
             h--;
         }
