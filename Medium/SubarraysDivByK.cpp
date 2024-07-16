b219373fe6c7cdf7953fe44855e0614aa86e6832
Yash Borkar <imyashb00@gmail.com>
2024-07-16 14:17:50 +0530

    Create SubarraysDivByK.cpp

---
+++ Medium/SubarraysDivByK.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int>mp;
+
+        int ans = 0;
+        int rem = 0;
+        int sum = 0;
+        mp[rem]++;
+
+        for(auto i:nums){
+            sum+=i;
+            rem = sum % k;
+            if(rem<0) rem+=k;   // For negative remainder
+            if(mp.find(rem)!=mp.end()) ans+=mp[rem];
+            mp[rem]++;
+        }
+
+        return ans;
+    }
+};
