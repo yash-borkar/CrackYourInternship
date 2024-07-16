45ff6ee7cc82b1aa55419b9c25148b478e3d63f1
Yash Borkar <imyashb00@gmail.com>
2024-07-16 12:14:31 +0530

    Create MoveZeroes.cpp

---
+++ Easy/MoveZeroes.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/move-zeroes/description/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        int index = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+               nums[index] = nums[i];
+               index++; 
+            }                   
+        }
+
+        for(int i=index; i<n; i++){
+            nums[i] = 0;
+        }
+    }
+};
