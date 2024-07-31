23c6c54203a8d8dbdd2bc8303350733bcf3bdb65
Yash Borkar <imyashb00@gmail.com>
2024-07-31 17:51:03 +0530

    Update FindDuplicate.cpp

--- Medium/FindDuplicate.cpp
+++ Medium/FindDuplicate.cpp
@@ -42,7 +42,6 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         //Positioning Method
-
         while(nums[0] != nums[nums[0]]){
             swap(nums[0], nums[nums[0]]);
         }
